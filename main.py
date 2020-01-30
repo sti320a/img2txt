82556492ee146590ca4733934f2b46ad9cbbeab2
Tomoaki Sugiayma <sti320a@gmail.com>
2020-01-30 22:27:39 +0900

    Create save_img and get_text

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import cv2
 import pyocr
 from PIL import Image
 
@@ -9,14 +10,54 @@ def get_ocr_tool():
     return tools[0]
 
 
+def save_img(
+        src_path: str,
+        out_path: str,
+        lang: str,
+        layout: int) -> None:
+    builder = pyocr.builders.WordBoxBuilder(
+        tesseract_layout=layout
+    )
+    ret = tool.image_to_string(
+        Image.open(src_path),
+        lang=lang,
+        builder=builder
+    )
+    out = cv2.imread(src_path)
+    for d in ret:
+        cv2.rectangle(
+            out,
+            d.position[0],
+            d.position[1],
+            (0, 0, 255),
+            2
+        )
+    cv2.imwrite(out_path, out)
+
+
+def get_text(
+        src_path: str,
+        out_path: str,
+        lang: str,
+        layout: int) -> str:
+    builder = pyocr.builders.TextBuilder(tesseract_layout=layout)
+    ret = tool.image_to_string(
+        Image.open(src_path),
+        lang=lang,
+        builder=builder
+    )
+    ret.replace(' ', '')
+    with open(out_path, 'w') as f:
+        f.write(ret)
+    return ret
+
+
 if __name__ == "__main__":
     tool = get_ocr_tool()
     if tool:
-        path = './images/book.png'
+        src = './images/book.png'
+        out = './images/ret.png'
         lang = 'jpn'
-        text = tool.image_to_string(
-            Image.open(path),
-            lang=lang,
-            builder=pyocr.builders.TextBuilder(tesseract_layout=6)
-        )
-        print(text.replace(' ', ''))
+        layout = 1
+        save_img(src, out, lang, layout)
+        get_text(src, out, lang, layout)
