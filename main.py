620fc62045a5b9c3acdd6b6fc22383032fa187d6
Tomoaki Sugiayma <sti320a@gmail.com>
2020-01-30 21:22:53 +0900

    Add ocr testing

---
+++ main.py
@@ -0,0 +1,22 @@
+import pyocr
+from PIL import Image
+
+
+def get_ocr_tool():
+    tools = pyocr.get_available_tools()
+    if len(tools) == 0:
+        return None
+    return tools[0]
+
+
+if __name__ == "__main__":
+    tool = get_ocr_tool()
+    if tool:
+        path = './images/test.png'
+        lang = 'jpn'
+        text = tool.image_to_string(
+            Image.open(path),
+            lang=lang,
+            builder=pyocr.builders.TextBuilder(tesseract_layout=6)
+        )
+        print(text)
